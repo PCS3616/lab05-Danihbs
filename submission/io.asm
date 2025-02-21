289adabeabd6564bd76864d2a76c7cffda293c50
Danihbs <163914611+Danihbs@users.noreply.github.com>
2025-02-21 23:33:31 +0000

    Add files via upload

--- submission/io.asm
+++ submission/io.asm
@@ -1,6 +1,7 @@
         GD  /000; Leitura dos primeiros dois dígitos
         MM  /0100
         GD  /000; Leitura do espaço 
+        MM  /060
         GD  /000; Leitura dos últimos dois dígitos
         MM  /0102
 
@@ -16,19 +17,21 @@
         MM  /0104; RESULTADO PARCIAL
         ML  /0202
         DV  /0202
-        SB  /000A
+        SB  /0204
         JN  /0300   
         LD  /0104
-        SB  /000A
-        AD  /0202
+        SB  /0204
+        AD  /0206
         AD  /0200
         PD  /100
-        HM  /0032
+        HM  /002E
 
 
         @   /0200
         K   /3030
-        K   /0100
+        K   /1000
+        k   /000A
+        k   /0100
 
         @   /0300
         LD  /0104
